c0a8c8dc247efeeca425f7f61c7d6555e7ddaa09
Joaobr200 <joaoarantesjob@gmail.com>
2020-08-27 22:42:27 -0300

    config redux-thunk

--- src/store/ducks/weather.js
+++ src/store/ducks/weather.js
@@ -1,5 +1,7 @@
+import api from "../../services/api";
+
 const types = {
-  FETCH: "weather/fetch",
+  FETCHING: "weather/fetch",
   SUCCESS: "weather/success",
   FAILURE: "weather/failure",
 };
@@ -8,16 +10,18 @@ const INITIAL_STATE = {
   loading: false,
   success: false,
   failure: false,
+  data: null,
 };
 
-const weatherReducer = (state = INITIAL_STATE, { type }) => {
+const weatherReducer = (state = INITIAL_STATE, { type, payload }) => {
   switch (type) {
-    case types.FETCH:
+    case types.FETCHING:
       return {
         ...state,
         loading: true,
         success: false,
         failure: false,
+        data: null,
       };
     case types.SUCCESS:
       return {
@@ -25,31 +29,73 @@ const weatherReducer = (state = INITIAL_STATE, { type }) => {
         loading: false,
         success: true,
         failure: false,
+        data: payload,
       };
     case types.FAILURE:
       return {
         ...state,
         loading: false,
         success: false,
-        failure: false,
+        failure: true,
+        data: null,
       };
     default:
-      return { ...state }
+      return { ...state };
   }
 };
 
-export const weatherActions = {
-  fetch: () => ({
-    type: types.FETCH,
+const weatherActions = {
+  fetching: () => ({
+    type: types.FETCHING,
   }),
 
-  success: () => ({
+  success: (payload) => ({
     type: types.SUCCESS,
+    payload,
   }),
 
   failure: () => ({
     type: types.FAILURE,
   }),
+};
+
+const { fetching, failure, success } = weatherActions;
+
+export const searchForWeather = async (dispatch, data) => {
+  dispatch(fetching());
+  try {
+    const request = await fetch(
+      `${api.url}q=${data}&appid=${api.key}&units=metric&lang=pt`
+    );
+
+    if (request.status === 404) {
+      dispatch(failure());
+    }
+
+    const json = await request.json();
+
+    if (json) {
+      const { name } = json;
+      const { main, description } = json.weather[0];
+      const { country } = json.sys;
+      const { temp } = json.main;
+
+      dispatch(
+        success([
+          {
+            name,
+            main,
+            description,
+            country,
+            temp,
+          },
+        ])
+      );
+    }
+  } catch (err) {
+    console.log(err);
+    dispatch(failure());
+  }
 };
 
 export default weatherReducer;
--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,7 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import ReduxThunk from "redux-thunk";
 import RootReducer from "./ducks";
 
-const store = createStore(RootReducer);
+const store = createStore(RootReducer, applyMiddleware(ReduxThunk));
 
 export default store;
